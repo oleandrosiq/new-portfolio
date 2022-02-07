f15c81d2422a13ed57ed44c5dd5b9b443b2b67e7
Leandrosiq <le_kts12@hotmail.com>
2022-02-06 22:46:28 -0300

    add section experience

---
+++ src/contents/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Experience } from '../Experience';
+
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Experience />
+    </Container>
+  );
+}
---
+++ src/contents/Home/styles.ts
@@ -0,0 +1,11 @@
+import { styled } from '../../styles/styled';
+
+export const Container = styled('div', {
+  width: '100%',
+  maxWidth: '95%',
+  margin: '0 auto',
+
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
