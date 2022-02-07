e70024b5a36337badf67f9c7e51efc9bc0cc0b3f
Leandrosiq <le_kts12@hotmail.com>
2022-02-06 21:16:32 -0300

    first commit in branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,3 @@
-import { GetServerSideProps } from "next";
-
 export default function Home() {
   return <h1>Hello World</h1>;
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  return {
-    props: {},
-    redirect: {
-      destination: '/me/bio',
-      permanent: true,
-    }
-  }
 }
