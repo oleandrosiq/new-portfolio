72d2e67eafb98d7852bfdb3bfb02d81d334d1fe9
Leandrosiq <le_kts12@hotmail.com>
2022-02-11 09:01:09 -0300

    add component Socials in home

--- src/contents/Home/styles.ts
+++ src/contents/Home/styles.ts
@@ -9,3 +9,57 @@ export const ContainerCenter = styled('div', {
   flexDirection: 'column',
   alignItems: 'center',
 });
+
+export const Socials = styled('div', {
+  width: '7rem',
+
+  borderRadius: '0.8rem 0 0 0.8rem',
+  background: 'var(--shape)',
+
+  position: 'fixed',
+  top: '50%',
+  right: '0',
+  transform: 'translateY(-50%)',
+
+  display: 'flex',
+  flexDirection: 'column',
+
+  '> p': {
+    width: '100%',
+    height:' 6rem',
+
+    borderRadius: '0.8rem 0 0 0',
+    background: '#2033fa',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    fontSize: '1.4rem',
+  },
+
+  '> a': {
+    width: '100%',
+    height: '6rem',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    gap: '0.5rem',
+
+    fontSize: '1.2rem',
+    color: 'var(--white)',
+
+    transition: 'all 0.2s ease-in-out',
+    cursor: 'pointer',
+
+    '&:last-child': {
+      borderRadius: '0 0 0 0.8rem',
+    },
+
+    '&:hover': {
+      filter: 'brightness(1.2)',
+    }
+  }
+});
