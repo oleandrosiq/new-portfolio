8f0294d963ae445826d8e0d285dc0984a8408483
Leandrosiq <le_kts12@hotmail.com>
2022-02-13 19:22:08 -0300

    add style stitches server side

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '../styles/styled';
 
 export default class MyDocument extends Document {
   render() {
@@ -8,8 +9,9 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
-          <meta charSet="utf-8" />
           <link rel="icon" type="image/svg" href="/favicon.svg" />
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
--- src/styles/styled.ts
+++ src/styles/styled.ts
@@ -1,2 +1,2 @@
 import { createStitches } from '@stitches/react';
-export const { styled, css, keyframes } = createStitches({});
+export const { styled, css, keyframes, getCssText } = createStitches({});
