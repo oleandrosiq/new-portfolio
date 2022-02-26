db23997f6753c917bc05d109b1e3754ce2a47c89
Leandrosiq <le_kts12@hotmail.com>
2022-02-26 10:16:57 -0300

    add alert home - Portfolio is under development.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,42 @@
-import React from 'react';
-import { 
-  FaGithub, 
-  FaLinkedin, 
-  FaInstagram, 
-} from 'react-icons/fa';
+import React, { useEffect } from 'react';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import Link from 'next/link';
 
 import { Home as HomeContent } from '../contents/Home';
 import { Socials } from '../contents/Home/styles';
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      <HomeContent />
-      <Socials>
-        <p>Sociais</p>
+	useEffect(() => {
+		alert('Portfolio is under development.');
+	}, []);
 
-        <Link href='https://www.instagram.com/oleandrosiq/?hl=pt-br' passHref>
-          <a target='_blank' style={{ background: '#000e7e' }}>
-            <FaInstagram size={22} color='var(--blue)' />
-            Instagram
-          </a>
-        </Link>
+	return (
+		<React.Fragment>
+			<HomeContent />
+			<Socials>
+				<p>Sociais</p>
 
-        <Link href='https://www.linkedin.com/in/leandrosiq1/' passHref>
-          <a target='_blank' style={{ background: '#00084e' }}>
-            <FaLinkedin size={22} color='var(--blue)' />
-            Linkedin
-          </a>
-        </Link>
+				<Link href="https://www.instagram.com/oleandrosiq/?hl=pt-br" passHref>
+					<a target="_blank" style={{ background: '#000e7e' }}>
+						<FaInstagram size={22} color="var(--blue)" />
+						Instagram
+					</a>
+				</Link>
 
-        <Link href='https://github.com/oleandrosiq' passHref>
-          <a target='_blank' style={{ background: '#00021f' }}>
-            <FaGithub size={22} color='var(--blue)' />
-            Github
-          </a>
-        </Link>
-      </Socials>
-    </React.Fragment>
-  );
+				<Link href="https://www.linkedin.com/in/leandrosiq1/" passHref>
+					<a target="_blank" style={{ background: '#00084e' }}>
+						<FaLinkedin size={22} color="var(--blue)" />
+						Linkedin
+					</a>
+				</Link>
+
+				<Link href="https://github.com/oleandrosiq" passHref>
+					<a target="_blank" style={{ background: '#00021f' }}>
+						<FaGithub size={22} color="var(--blue)" />
+						Github
+					</a>
+				</Link>
+			</Socials>
+		</React.Fragment>
+	);
 }
